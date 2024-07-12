0cf2af8b8caad895a8d7c838676945057dbbabe2
pannu2107 <amanpannu2107@gmail.com>
2024-07-12 14:25:54 +0530

    Add files via upload

---
+++ M71.cpp
@@ -0,0 +1,41 @@
+Assignment no. 1
+
+Question 1
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Physics"<<endl<<"Wallah";
+}
+
+Question 2
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<5+5;
+}
+
+Question 3
+int take 4 bytes
+bool take 1 bytes
+float take 4 bytes
+
+Question 4
+a=6 , b= 4 are answer but the variable in end is a=6 , b=3
+
+Question 5
+#include<iostream>
+using namespace std;
+int main(){
+    int radius=10;
+    float pi=3.1415;
+    float C = (2*pi*radius);
+    cout<<"Circumfrance of a circle : "<<C;
+}
+
+Question 6
+1st can't be a variable because it start with number
+2st is also a variable 
+3rd is also a variable
+4th is also a variable
+5st can't be a variable because it has a gap
+
